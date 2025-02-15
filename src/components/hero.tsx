af47609a42a9b8d417f58aa03db573c9f9ffdc85
xootzie <xootzie@gmail.com>
2025-02-14 23:55:05 -0500

    dsasad

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -187,7 +187,7 @@ loadstring(game:HttpGet("https://luau.tech/build"))()`;
                     <pre className="text-sm text-blue-400 font-mono whitespace-pre overflow-x-auto">
                     <code>
                     <span className="text-blue-300">skipGameCheck</span> <span className="text-gray-400">=</span> <span className="text-red-400">false</span>
-                    <span className="text-purple-400">{"\n"}loadstring</span>(<span className="text-blue-300">game</span><span className="text-gray-400">:</span><span className="text-yellow-400">HttpGet</span>(<span className="text-green-400">"https://luau.tech/build"</span>))()
+                    <span className="text-purple-400">{"\n"}loadstring</span>(<span className="text-blue-300">game</span><span className="text-gray-400">:</span><span className="text-yellow-400">HttpGet</span>(<span className="text-green-400">&quot;https://luau.tech/build&quot;</span>))()
 </code>
                     </pre>
                   </div>
