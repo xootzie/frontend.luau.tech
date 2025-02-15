1e4c5c16de47fb193c7a413d372cf747ed6cb2b3
xootzie <xootzie@gmail.com>
2025-02-15 01:37:02 -0500

    sdadada

--- src/app/key/page.tsx
+++ src/app/key/page.tsx
@@ -377,7 +377,7 @@ export default function KeySystem() {
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
                   d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
               </svg>
-              <p className="text-sm">Each key will last for 12 hours.</p>
+              <p className="text-sm">Each key will last for 48 hours.</p>
             </div>
           </div>
         </div>
